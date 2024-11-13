b2018fb79922a0c99a7f479e7730368498f13fbd
= <matte.campa@yahoo.it>
2024-11-13 18:33:27 +0100

    non working commit

--- MNIST/MLP.py
+++ MNIST/MLP.py
@@ -5,6 +5,10 @@ from jax import random
 from jax.tree_util import tree_map
 from jax.nn import relu, softmax, sigmoid, log_softmax
 from jax.scipy.special import logsumexp
+from functools import partial
+from ETL import ETL, NumpyDataLoader
+import time
+import pickle
 
 '''
 This code is highly inspired by the tutorial "Get Started with JAX" by Aleksa Gordić.
@@ -12,58 +16,101 @@ Refer to Gordić, Aleksa. "Get started with JAX." GitHub repository, 2021.
 Available at: https://github.com/gordicaleksa/get-started-with-JAX
 '''
 
-def MLP_init(layers : list) -> None:
-    params = []
-    seed = 0 
-    parent_key = random.PRNGKey(seed)
+class MLP:
+    def __init__(self, layers : list, seed : int = 0):
+        """
+            MLP Constructor
+            layers : list, contains the number of neurons in each layer
+        """
 
-    keys = random.split(parent_key, len(layers)-1)
-    scale = 0.01 # reduce the initial variance of weigths
-    for n_in,n_out,key in zip(layers[:-1],layers[1:],keys):
-        params.append({
-            'W': scale*random.normal(key, (n_in,n_out)),
-            'b': scale*jnp.zeros(n_out)
-        })
+        self.__params = []
+        self.__parent_key = random.PRNGKey(seed)
 
-    return params
+        keys = random.split(self.__parent_key, len(layers)-1)
+        scale = 0.1 # reduce the initial variance of weigths
+        for n_in,n_out,key in zip(layers[:-1],layers[1:],keys):
+            self.__params.append({
+                'W': scale*random.normal(key, (n_in,n_out)),
+                'b': scale*jnp.zeros(n_out)
+            })
 
-@jit
-def MLP_predict(params, x):
-    hidden_layers = params[:-1]
+    @partial(jit, static_argnums=(0,))
+    def __predict(self, x : jnp.array, params : list) -> jnp.array:
+        """
+            Forward pass of the MLP. Works only for single inputs, in fact
+            it'll be wrapped by batched_predict in the next function. 
+            x : jnp.array, input array
+            params : list, containing the model parameters
+        """
+        hidden_layers = params[:-1]
 
-    act = x
-    for layer in hidden_layers:
-        act = jnp.dot(act, layer['W']) + layer['b']
-        act = relu(act)
-    
-    w_last, b_last = params[-1]['W'], params[-1]['b']
-    act = jnp.dot(act, w_last) + b_last
-    return act - logsumexp(act)
-
-batched_MLP_predict = vmap(MLP_predict, in_axes=(None, 0))
-
-def loss_fn(params, imgs, labels):
-    pred = batched_MLP_predict(params, imgs)
-    return -jnp.mean(labels*pred)
-
-def accuracy(params, imgs, labels):
-    pred = batched_MLP_predict(params, imgs)
-    return jnp.mean(jnp.argmax(pred, axis=1) == jnp.argmax(labels, axis=1))
-
-@jit
-def update(params, imgs, labels, lr=1e-3):
-    loss,grads = value_and_grad(loss_fn)(params, imgs, labels)
-    return loss, tree_map(lambda p,g: p - lr*g, params, grads)
-
-def train(params, imgs, labels, epochs = 100, lr=1e-3):
-    for epoch in range(epochs):
-        loss, grads = value_and_grad(loss_fn)(params, imgs, labels)
-        params = tree_map(lambda p,g: p - lr*g, params, grads)
+        act = x
+        for layer in hidden_layers:
+            act = jnp.dot(act, layer['W']) + layer['b']
+            act = relu(act)
         
-        if epoch % 100 == 0:
-            print(f"Epoch {epoch}, loss {loss}, accuracy {accuracy(params, imgs, labels)}")
+        w_last, b_last = params[-1]['W'], params[-1]['b']
+        act = jnp.dot(act, w_last) + b_last
+        return act - logsumexp(act)
 
-'''
+    def batched_predict(self, x : jnp.arange, params : list =None) -> jnp.array:
+        """
+            Batched version of self.__predict. It either use the current
+            parameters or the ones passed as argument. This is key for 
+            training, since paramaters are updated at each iteration.
+
+        """
+        params = params if params is not None else self.__params
+        return vmap(self.__predict, in_axes=(0, None))(x, params)
+
+    def loss_function(self, params : list, imgs : jnp.array, labels : jnp.array) -> jnp.array:
+        """
+            Loss function for the MLP. It computes the cross-entropy loss
+            for a batch of data.
+            params : list, model parameters
+            imgs : jnp.array, batch of images
+            labels : jnp.array, batch of labels
+        """
+        pred = self.batched_predict(imgs, params)
+        return -jnp.mean(labels*pred)    
+
+    @partial(jit, static_argnums=(0,))
+    def _update(self, params : list, imgs : jnp.array, labels : jnp.array, lr : float) -> tuple:
+        """
+            Parameters update function. It takes as input as batch of data 
+            and performs single step batch gradient descent.
+            params : list, model parameters
+            imgs : jnp.array, batch of images
+            labels : jnp.array, batch of labels
+            lr : float, learning rate
+        """
+        loss,grads = value_and_grad(self.loss_function)(params, imgs, labels)
+        return loss, tree_map(lambda p,g: p - lr*g, params, grads)
+    
+    def train(self, train_loader : NumpyDataLoader, epochs : int = 100, lr : float = 1e-3) -> None:
+        """
+            Training method. Could I jit this?
+            train_loader : NumpyDataLoader, custom loader object
+            epochs : int, number of epochs
+            lr : float, learning rate
+        """
+        for epoch in range(epochs):
+            for cnt,(img,label) in enumerate(train_loader):
+                loss, self.__params = self._update(self.__params, img, label, lr)
+                if cnt % 100 == 0:
+                    print(f"Epoch {epoch}, batch {cnt}, loss {loss}")
+
+    @property
+    def params(self):
+        """
+            Getter function for MLP parameters, return in pytree format
+        """
+        return tree_map(lambda x : x.shape, self.__params)
+        
+    def save_model(self, path):
+        with open(path, 'wb') as f:
+            pickle.dump(self.__params, f)
+
 if __name__=="__main__":
     etl = ETL("mnist.npz")
     X_train,y_train,X_test,y_test = etl.loadData(reduce=False)
@@ -71,11 +118,10 @@ if __name__=="__main__":
     batch_size = 128
     train_loader = NumpyDataLoader(X_train, y_train, batch_size, shuffle=True, drop_last=True)
 
-    mlp = MLP_init([784,128,128,10])
+    mlp = MLP([784,128,128,10])
+    
+    mlp.train(train_loader, epochs=10)
 
-    for epoch in range(50):
-        for cnt,(img,label) in enumerate(train_loader):
-            loss, mlp = update(mlp, img, label)
-            if cnt % 100 == 0:
-                print(f"Epoch {epoch}, batch {cnt}, loss {loss}")
-'''
+    print("Training error: ", mlp.loss_function(mlp.params, X_train, y_train))
+
+    mlp.save_model("mlp_model.pkl")
--- MNIST/MLP_wip.py
+++ MNIST/MLP_wip.py
@@ -7,118 +7,65 @@ from jax.nn import relu, softmax, sigmoid, log_softmax
 from jax.scipy.special import logsumexp
 from functools import partial
 
+import pickle
+
 '''
 This code is highly inspired by the tutorial "Get Started with JAX" by Aleksa Gordić.
 Refer to Gordić, Aleksa. "Get started with JAX." GitHub repository, 2021.
 Available at: https://github.com/gordicaleksa/get-started-with-JAX
 '''
 
-class MLP:
-    def __init__(self,layers : list) -> None:
-        self.__params = []
-        self.__activations_fn = [None]*(len(layers)-1)
-        self.__loss_fn = None
-        self.__optimizer = None
-        self.__metrics = []
-        self.__seed = 0 
-        self.__parent_key = random.PRNGKey(self.__seed)
+def MLP_init(layers : list) -> None:
+    params = []
+    seed = 0 
+    parent_key = random.PRNGKey(seed)
 
-        keys = random.split(self.__parent_key, len(layers)-1)
-        scale = 0.01 # reduce the initial variance of weigths
-        for n_in,n_out,key in zip(layers[:-1],layers[1:],keys):
-            self.__params.append({
-                'W': scale*random.normal(key, (n_in,n_out)),
-                'b': scale*jnp.zeros(n_out)
-            })
+    keys = random.split(parent_key, len(layers)-1)
+    scale = 0.01 # reduce the initial variance of weigths
+    for n_in,n_out,key in zip(layers[:-1],layers[1:],keys):
+        params.append({
+            'W': scale*random.normal(key, (n_in,n_out)),
+            'b': scale*jnp.zeros(n_out)
+        })
 
-    @property
-    def params(self) -> list:
-        return self.__params
+    return params
 
-    def set_activations(self, activations : list) -> None:
-        if len(activations) != len(self.__activations_fn):
-            raise ValueError("Number of activations must match the number of layers")
+@jit
+def MLP_predict(params, x):
+    hidden_layers = params[:-1]
+
+    act = x
+    for layer in hidden_layers:
+        act = jnp.dot(act, layer['W']) + layer['b']
+        act = relu(act)
+    
+    w_last, b_last = params[-1]['W'], params[-1]['b']
+    act = jnp.dot(act, w_last) + b_last
+    return act - logsumexp(act)
+
+batched_MLP_predict = vmap(MLP_predict, in_axes=(None, 0))
+
+def loss_fn(params, imgs, labels):
+    pred = batched_MLP_predict(params, imgs)
+    return -jnp.mean(labels*pred)
+
+def accuracy(params, imgs, labels):
+    pred = batched_MLP_predict(params, imgs)
+    return jnp.mean(jnp.argmax(pred, axis=1) == jnp.argmax(labels, axis=1))
+
+@jit
+def update(params, imgs, labels, lr=1e-3):
+    loss,grads = value_and_grad(loss_fn)(params, imgs, labels)
+    return loss, tree_map(lambda p,g: p - lr*g, params, grads)
+
+def train(params, imgs, labels, epochs = 100, lr=1e-3):
+    for epoch in range(epochs):
+        loss, grads = value_and_grad(loss_fn)(params, imgs, labels)
+        params = tree_map(lambda p,g: p - lr*g, params, grads)
         
-        for i, activation in enumerate(activations):
-            if activation == "relu":
-                self.__activations_fn[i] = relu
-            elif activation == "sigmoid":
-                self.__activations_fn[i] = sigmoid
-            elif activation == "softmax":
-                self.__activations_fn[i] = softmax
-            elif activation == "log_softmax":
-                self.__activations_fn[i] = log_softmax
-            elif activation == "sigmoid":
-                self.__activations_fn[i] = sigmoid
-            elif activation == "tanh":
-                self.__activations_fn[i] = jnp.tanh
-            elif activation == "identity":
-                self.__activations_fn[i] = lambda x: x
-            else:
-                raise ValueError(f"Activation function {activation} not supported")
-            
-        # cast to tuple
-        self.__activations_fn = tuple(self.__activations_fn)
+        if epoch % 100 == 0:
+            print(f"Epoch {epoch}, loss {loss}, accuracy {accuracy(params, imgs, labels)}")
 
-    @jit
-    def predict_jit(params, x):
-        #if activations_fn[0] is None:
-        #    raise Warning("Activations not set")
-
-        act = x
-        for i,layer in enumerate(params[:-1]):
-            act = jnp.dot(act,layer['W']) + layer['b']
-            act = relu(act)
-
-        w_last, b_last = params[-1]['W'], params[-1]['b']
-        act = jnp.dot(act,w_last) + b_last
-
-        return act - logsumexp(act)
-
-    def predict(self, params, x):
-        batched_predict = vmap(self.predict_jit, in_axes=(None,0))
-        return batched_predict(params, x)
-
-    def loss_fn(self,params,imgs,labels):
-        pred = self.predict(x = imgs, params=params)
-        return -jnp.mean(pred * labels)
-    
-    # this method does not work
-    def accuracy(self,imgs,labels):
-        pred = self.predict_batch(imgs)
-        return jnp.mean(jnp.argmax(pred,axis=1) == jnp.argmax(labels,axis=1))
-
-    @staticmethod
-    @partial(jit, static_argnames=['loss_fn'])
-    def __update_jit(loss_fn, params, imgs, labels,lr=0.01):
-        loss,grads = value_and_grad(loss_fn)(params,imgs,labels)
-        return loss,tree_map(lambda p,g: p-lr*g, params, grads)
-    
-    def train(self, X_train, y_train, epochs=100, lr=0.01):
-        params = self.__params
-
-        for i in range(epochs):
-            loss, params = self.__update_jit(self.loss_fn, params, X_train, y_train, lr)
-            
-            if i % 10 == 0:
-                print(f"Epoch {i}, Loss: {loss}")
-            
-        self.__params = params
-
-    def print(self) -> None:
-        print(f"MLP with {len(self.__params)} layers")
-        print("-"*30)
-        print(tree_map(lambda x: x.shape, self.__params))
-
-from ETL import ETL
-
-if __name__=="__main__":
-    mlp = MLP([784,128,128,10])
-    
-    etl = ETL("mnist.npz")
-    X_train, X_test, y_train, y_test = etl.loadData(reduce=True)
-    img = X_train[0]
-
-    print(img.shape)
-    pred = mlp.predict(mlp.params,img)
-    print(pred.shape)
+def save_model(params, path):
+    with open(path, 'wb') as f:
+        pickle.dump(params, f)
